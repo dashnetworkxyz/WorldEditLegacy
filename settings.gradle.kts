f361853cf06334b343be28158a94e6b6fa4a07bb
MasterDash5 <github@masterdash5.com>
2026-05-12 15:24:36 -0600

    Revert "start move to kotlin for gradle"
    This reverts commit cfd6fef7d56608589bdbc754c910d07a17bd36d9.

--- settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
-rootProject.name = "worldedit"
-include("worldedit-core")
-include("worldedit-bukkit")
-include("worldedit-forge")
-//include("worldedit-sponge") TODO: No sponge for now
